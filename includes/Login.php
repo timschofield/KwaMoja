d878795a3afa07c90abe17b4c744fc5dc898d8bf
Tim Schofield <tim@weberpafrica.com>
2021-12-06 14:48:08 +0000

    Improvements to the login screen
    Files changed in commit:
    css/eye.png
    css/login.css
    includes/Login.php
    javascripts/Login.js
    On branch NewInterface
    
    New custom select box including:
     A logo image from the companies folder
     options have a customisable Background/Foreground
    Toggle password box to show password

--- includes/Login.php
+++ includes/Login.php
@@ -2,65 +2,78 @@
 // Display demo user name and password within login form if $AllowDemoMode is true
 include ($PathPrefix . 'includes/LanguageSetup.php');
 include ('LanguagesArray.php');
-include ('MobileDetect.php');
-$MobileDetect = new Mobile_Detect;
+
 if ((isset($AllowDemoMode)) and ($AllowDemoMode == True) and (!isset($demo_text))) {
 	$demo_text = _('Login as user') . ': <i>' . _('admin') . '</i><br />' . _('with password') . ': <i>' . _('kwamoja') . '</i>';
-} elseif (!isset($demo_text)) {
-	$demo_text = _('Please login here');
-}
+	} elseif (!isset($demo_text)) {
+		$demo_text = _('Please login here');
+	}
 
-//echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
-echo '<html>
+	echo '<!DOCTYPE html>';
+	echo '<html>
 		<head>
-			<title>' . $ProjectName . ' ' . _('Login screen') . '</title>';
-echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-echo '<link rel="shortcut icon" href="favicon.ico?v=2" type="image/x-icon" />';
+			<title>', $ProjectName, ' ', _('Login screen'), '</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link rel="shortcut icon" href="favicon.ico?v=2" type="image/x-icon" />
+			<script async type="text/javascript" src = "', $PathPrefix, $RootPath, '/javascripts/Login.js"></script>';
 
-if ($LanguagesArray[$DefaultLanguage]['Direction'] == 'rtl') {
-	echo '<link rel="stylesheet" href="css/login_rtl.css" type="text/css" />';
-} else {
-	//	echo '<link rel="stylesheet" href="css/login-mobile.css" type="text/css" />';
-	echo '<link rel="stylesheet" href="css/login.css" type="text/css" />';
-}
-?>
-</head>
-<body>
+	if ($LanguagesArray[$DefaultLanguage]['Direction'] == 'rtl') {
+		echo '<link rel="stylesheet" href="css/login_rtl.css" type="text/css" />';
+	} else {
+		echo '<link rel="stylesheet" href="css/login.css" type="text/css" />';
+	}
+	echo '</head>';
 
-<div id="container">
-	<table>
-		<tr>
-			<th colspan="2">
-				<div id="login_logo">
-					<a href="<?php echo $HomePage; ?>" target="_blank"><img src="css/<?php echo $DefaultDatabase; ?>.png" style="width:100%" /></a>
-				</div>
-			</th>
-		</tr>
-		<tr>
-			<td id="login-container">
-				<div id="login_box">
-					<form action="index.php" name="LogIn" method="post" class="noPrint">
-					<input type="hidden" name="FormID" value="<?php
-echo $_SESSION['FormID'];
-?>" />
-					<label><?php
-echo _('Company');
-?>:</label>
+	echo '<body>
+		<div id="container">
+			<div id="login_logo">
+				<a href="', $HomePage, '" target="_blank"><img src="css/', $DefaultDatabase, '.png" style="width:100%" /></a>
+			</div>
+			<div id="login_box">
+				<form action="index.php" name="LogIn" method="post" class="noPrint">
+				<input type="hidden" name="FormID" value="', $_SESSION['FormID'], '" />';
 
-					<?php
-if (isset($_COOKIE['Login'])) {
-	$DefaultCompany = $_COOKIE['Login'];
-}
-if ($AllowCompanySelectionBox === 'Hide') {
-	// do not show input or selection box
-	echo '<input type="hidden" name="CompanyNameField"  value="' . $DefaultCompany . '" />';
-} else if ($AllowCompanySelectionBox === 'ShowInputBox') {
-	// show input box
-	echo '<input type="text" required="required" autofocus="autofocus" name="CompanyNameField"  value="' . $DefaultCompany . '" />';
-} else {
-	// Show selection box ($AllowCompanySelectionBox == 'ShowSelectionBox')
-	echo '<select name="CompanyNameField">';
+	if (isset($_COOKIE['Login'])) {
+		$DefaultCompany = $_COOKIE['Login'];
+	}
+	if ($AllowCompanySelectionBox === 'Hide') {
+		// do not show input or selection box
+		echo '<input type="hidden" name="CompanyNameField"  value="' . $DefaultCompany . '" />';
+	} else if ($AllowCompanySelectionBox === 'ShowInputBox') {
+		// show input box
+		echo '<input type="text" required="required" autofocus="autofocus" name="CompanyNameField"  value="' . $DefaultCompany . '" />';
+	} else {
+		// Show selection box ($AllowCompanySelectionBox == 'ShowSelectionBox')
+		echo '<select name="CompanyNameField" id="CompanyNameField">';
+
+		$DirHandle = dir('companies/');
+
+		while (false !== ($CompanyEntry = $DirHandle->read())) {
+			if (is_dir('companies/' . $CompanyEntry) and $CompanyEntry != '..' and $CompanyEntry != '' and $CompanyEntry != '.' and $CompanyEntry != 'default') {
+				if (file_exists('companies/' . $CompanyEntry . '/Companies.php')) {
+					include ('companies/' . $CompanyEntry . '/Companies.php');
+				} else {
+					$CompanyName[$CompanyEntry] = $CompanyEntry;
+				}
+				if ($CompanyEntry == $DefaultCompany) {
+					echo '<option selected="selected" value="' . $CompanyEntry . '">' . $CompanyName[$CompanyEntry] . '</option>';
+				} else {
+					echo '<option value="' . $CompanyEntry . '">' . $CompanyName[$CompanyEntry] . '</option>';
+				}
+			}
+		}
+
+		$DirHandle->close();
+
+		echo '</select>';
+	}
+
+	echo '<label for="dropdownlist">', _('Company'), ':</label>';
+
+	echo '<input type="text" id="CompanySelect" readonly value="' . $CompanyName[$DefaultCompany] . '" />';
+
+	echo '<ol id="dropdownlist" class="dropdownlist">';
 
 	$DirHandle = dir('companies/');
 
@@ -72,48 +85,36 @@ if ($AllowCompanySelectionBox === 'Hide') {
 				$CompanyName[$CompanyEntry] = $CompanyEntry;
 			}
 			if ($CompanyEntry == $DefaultCompany) {
-				echo '<option selected="selected" value="' . $CompanyEntry . '">' . $CompanyName[$CompanyEntry] . '</option>';
+				echo '<li class="option" id="' . $CompanyEntry . '" ><img id="optionlogo" src="companies/' . $CompanyEntry . '/logo.png" /><span id="optionlabel">', $CompanyName[$CompanyEntry], '</span></li>';
 			} else {
-				echo '<option value="' . $CompanyEntry . '">' . $CompanyName[$CompanyEntry] . '</option>';
+				echo '<li class="option" id="' . $CompanyEntry . '" ><img id="optionlogo" src="companies/' . $CompanyEntry . '/logo.png" /><span id="optionlabel">', $CompanyName[$CompanyEntry], '</span></li>';
 			}
 		}
 	}
-
 	$DirHandle->close();
 
-	echo '</select>';
-}
-?>
+	echo '</ol>';
 
-					<br />
-					<label><?php
-echo _('User name');
-?>:</label>
-					<input type="text" autofocus="autofocus" required="required" name="UserNameEntryField" placeholder="<?php echo _('User name'); ?>" maxlength="20" /><br />
-					<label><?php
-echo _('Password');
-?>:</label>
-					<input type="password" required="required" name="Password" placeholder="<?php echo _('Password'); ?>" />
-	   <div id="demo_text">
-	   <?php
-if (isset($demo_text)) {
-	echo $demo_text;
-}
-?>
-	   </div>
-					<button class="button" type="submit" value="<?php
-echo _('Login');
-?>" name="SubmitUser">
-					<?php
-echo _('Login');
-?>
-					 <img src="css/tick.png" title="' . _('Upgrade') . '" alt="" class="ButtonIcon" /></button>
-					</form>
-				</div>
-			</td>
-		</tr>
-	</table>
-</div>
+	echo '<label>', _('User name'), ':</label>
+		<input type="text" autocomplete="username" autofocus="autofocus" required="required" name="UserNameEntryField" placeholder="', _('User name'), '" maxlength="20" /><br />
+		<label>', _('Password'), ':</label>
+		<input type="password" autocomplete="current-password" id="password" required="required" name="Password" placeholder="', _('Password'), '" />
+		<input type="text" id="eye" readonly title="', _('Show Password'), '" />
+		<div id="demo_text">';
 
-</body>
-</html>
+	if (isset($demo_text)) {
+		echo $demo_text;
+	}
+
+	echo '</div>';
+
+	echo '<button class="button" type="submit" value="', _('Login'), '" name="SubmitUser">', _('Login'), ' ', '<img src="css/tick.png" title="', _('Login'), '" alt="" class="ButtonIcon" />
+	</button>';
+
+	echo '</form>
+	</div>
+</div>';
+
+	echo '</body>
+	</html>';
+?>
---
+++ javascripts/Login.js
@@ -0,0 +1,55 @@
+function ShowCompanies() {
+	document.getElementById("dropdownlist").addEventListener("transitionend",
+		function() {
+			if (document.getElementById("dropdownlist").style.overflow=="hidden" || document.getElementById("dropdownlist").style.overflow=="") {
+				document.getElementById("dropdownlist").style.overflow="auto";
+			} else {
+				document.getElementById("dropdownlist").style.overflow="hidden";
+			}
+		}
+	);
+	document.getElementById("dropdownlist").style.transition = "max-height 0.3s";
+	if (document.getElementById("dropdownlist").style.maxHeight=="0px" || document.getElementById("dropdownlist").style.maxHeight=="") {
+		rect=document.getElementById("CompanySelect").getBoundingClientRect();
+		var ViewPortHeight = window.innerHeight;
+		var DropDownTop=rect.bottom;
+		document.getElementById("dropdownlist").style.left=rect.left;
+		document.getElementById("dropdownlist").style.maxHeight=ViewPortHeight-DropDownTop-10;
+		document.getElementById("CompanySelect").style.background = "url(\'css/ascending.png\') no-repeat right transparent";
+		document.getElementById("CompanySelect").style.backgroundSize = "contain";
+	} else {
+		document.getElementById("dropdownlist").style.overflow="hidden"
+		document.getElementById("dropdownlist").style.maxHeight="0px";
+		document.getElementById("CompanySelect").style.background = "url(\'css/descending.png\') no-repeat right transparent";
+		document.getElementById("CompanySelect").style.backgroundSize = "contain";
+	}
+}
+function UpdateSelect() {
+	document.getElementById("CompanyNameField").value=this.id;
+	document.getElementById("CompanySelect").value=document.getElementById("CompanyNameField").options[document.getElementById("CompanyNameField").selectedIndex].text;
+	document.getElementById("dropdownlist").style.maxHeight="0px"
+	document.getElementById("CompanySelect").style.background = "url(\'css/descending.png\') no-repeat right transparent";
+	document.getElementById("CompanySelect").style.backgroundSize = "contain";
+}
+function TogglePassword () {
+	if (document.getElementById("password").type == "password") {
+		document.getElementById("password").type = "text";
+	} else {
+		document.getElementById("password").type = "password";
+	}
+}
+function checkMousePos(event) {
+	if (document.getElementById("dropdownlist").style.maxHeight!="0px" && document.getElementById("dropdownlist").style.maxHeight!="") {
+		rect=document.getElementById("CompanySelect").getBoundingClientRect();
+		if ((event.clientX < rect.left || event.clientX > rect.right) || (event.clientY < rect.top || event.clientY > rect.bottom)) {
+			ShowCompanies();
+		}
+	}
+}
+document.addEventListener("click", checkMousePos);
+document.getElementById("eye").addEventListener("click", TogglePassword);
+document.getElementById("CompanySelect").addEventListener("click", ShowCompanies);
+var options=document.getElementsByClassName("option");
+for (i = 0; i < options.length; i++) {
+	document.getElementById(options[i].id).addEventListener("click", UpdateSelect);
+}
