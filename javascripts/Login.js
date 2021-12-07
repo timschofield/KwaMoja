548f7e5ae55d918c8737db706a812b7356663566
Tim Schofield <tim@weberpafrica.com>
2021-12-07 14:58:18 +0000

    Fix positioning issues
    Files changed in commit:
    css/login.css
    javascripts/Login.js
    On branch NewInterface

--- javascripts/Login.js
+++ javascripts/Login.js
@@ -13,8 +13,8 @@ function ShowCompanies() {
 		rect=document.getElementById("CompanySelect").getBoundingClientRect();
 		var ViewPortHeight = window.innerHeight;
 		var DropDownTop=rect.bottom;
-		document.getElementById("dropdownlist").style.left=rect.left;
-		document.getElementById("dropdownlist").style.maxHeight=ViewPortHeight-DropDownTop-10;
+		document.getElementById("dropdownlist").style.left=rect.left+"px";
+		document.getElementById("dropdownlist").style.maxHeight=ViewPortHeight-DropDownTop-10+"px";
 		document.getElementById("CompanySelect").style.background = "url(\'css/ascending.png\') no-repeat right transparent";
 		document.getElementById("CompanySelect").style.backgroundSize = "contain";
 	} else {
